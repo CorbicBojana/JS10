96b3d8fe016832f759f985bc114aefbdd5f56bbc
CorbicBojana <corbicbojana@gmail.com>
2020-06-18 15:11:37 +0200

    fix

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,7 @@
-var mobileButton = document.getElementById("nav_mobile_button");
-var mobileButtonOpen = document.getElementById("nav_mobile_button_open");
-var nav = document.getElementById("nav");
-var openNav = false;
+const mobileButton = document.getElementById("nav_mobile_button");
+const mobileButtonOpen = document.getElementById("nav_mobile_button_open");
+const nav = document.getElementById("nav");
+let openNav = false;
 
 mobileButton.addEventListener("click", function() {
     if (openNav === false) {
@@ -21,12 +21,65 @@ mobileButton.addEventListener("click", function() {
 
 var sliderButton = document.getElementsByClassName("slider_button_link");
 
-for (var i=0; i < sliderButton.length; i++) {
-    sliderButton[i].addEventListener("click", function(e) {
+function sliderButtonChange() {
+    let i = 0;
 
-        for (var y=0; y < sliderButton.length; y++) {
-            sliderButton[y].classList.remove("active");
-            e.target.classList.add("active");
+    setInterval(function() {
+
+        for (let x=0; x < sliderButton.length; x++) {
+            sliderButton[x].classList.remove("active");
         }
-    })
-}
+
+        sliderButton[i].classList.add("active");
+    
+        if (i === 3) {
+            i = 0
+        }
+        else {
+            i++;
+           }
+        }, 3000)
+};
+
+sliderButtonChange();
+
+// Check email format
+const form = document.getElementById("form");
+const email = document.getElementById("form_input");
+
+// Show error message
+function showError(message) {
+    var small = document.getElementById("small");
+    small.innerText = message;
+  }
+  
+// Hide error message
+function showSucces(input) {
+    //
+  }
+
+// Check required fields
+function checkRequired(input) {
+      if (input.value.trim() === "") {
+        showError("Cannot be empty");
+      } else {
+        showSucces(input);
+      }
+  }
+
+function checkEmail(input) {
+    var reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  
+    if (reg.test(input.value.trim())) {
+      showSucces(input);
+    } else {
+      showError("Looks like this is not an email");
+    }
+  }
+  
+  form.addEventListener("submit", function(e, input) {
+    e.preventDefault();
+  
+    checkRequired(email);
+    checkEmail(email);
+  });
